b3354bd0fb87d3557f595b0d1c750924cdd287fd
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-12-04 23:45:00 -0600

    draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	var gifs = ["Joe", "Sarah", "Evan", "Eric", "Katie", "Tom", "Erin", "Ali", "Ally", "Jordan", "Blake"];
 	
+  //Displays 10 gifs that are based on the button clicked.
 	function displayGiphyInfo() {
-		
 		
 	    var gif = $(this).attr("data-name");
 	   	var queryURL = "https://api.giphy.com/v1/gifs/search?q= " + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 		
+    //call the API using AJAX.
     $("#gif-view").empty();
 	    $.ajax({
 	      url: queryURL,
@@ -16,39 +17,50 @@ $(document).ready(function() {
 	    	$.each(results, function(index, value){
           var image = $("<img>").attr({
             "data-state": "still",
-            "src": results[index].images.fixed_width_downsampled.url,
-            "data-animate": results[index].images.fixed_width_downsampled.url,
-            "data-still": results[index].images.fixed_width_downsampled.url, 
+            "src": results[index].images.fixed_width_still.url,
+            "data-animate": results[index].images.fixed_width.url,
+            "data-still": results[index].images.fixed_width_still.url, 
           })
+        console.log(response)
            $("#gif-view").prepend(image);
-        }) 
- 		   });
-  } 
-      
-      displayGiphyInfo();
 
-      function renderButtons() {
-
-        // Deleting the buttons prior to adding new gifs
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-
-        // Looping through the array of gifs
-        for (var i = 0; i < gifs.length; i++) {
-
-          // Then dynamicaly generating buttons for each gif in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of gif to our button
-          a.addClass("gif");
-          // Adding a data-attribute
-          a.attr("data-name", gifs[i]);
-          // Providing the initial button text
-          a.text(gifs[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
+        
+        
+      })   
+    //On 'Click' function for animating the GIFS
+        function animateClick(index, value){
+          $("img").attr("data-animate");
         }
-      }
+          $("img").on("click", animateClick();
+
+	   });
+  } 
+  //Call the display GIF function
+  displayGiphyInfo();
+
+  //Function for rendering our array into buttons
+  function renderButtons() {
+
+    // Deleting the buttons prior to adding new gifs
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // Looping through the array of gifs
+    for (var i = 0; i < gifs.length; i++) {
+
+      // Then dynamicaly generating buttons for each gif in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      var a = $("<button>");
+      // Adding a class of gif to our button
+      a.addClass("gif");
+      // Adding a data-attribute
+      a.attr("data-name", gifs[i]);
+      // Providing the initial button text
+      a.text(gifs[i]);
+      // Adding the button to the buttons-view div
+      $("#buttons-view").append(a);
+    }
+  }
 
       // This function handles events where one button is clicked
       $("#add-gif").on("click", function(event) {
